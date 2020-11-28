5b56ce7c755dc4f49475a7d5e2816f36c0e0179b
anonymous-31 <57583996+anonymous-31@users.noreply.github.com>
2020-11-28 21:45:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import pyttsx3
+import PyPDF2
+from PyPDF2 import PdfFileWriter, PdfFileReader
+book = open('mindset.pdf','rb')
+pdfreader = PyPDF2.PdfFileReader(book)
+pages = pdfreader.numPages
+print(pages)
+speaker = pyttsx3.init()
+page= pdfreader.getPage(31)
+text = page.extractText()
+print(text)
+# speaker.say("text")
+# name = "Dharmendra thagaiya"
+rate = speaker.getProperty('rate')   # getting details of current speaking rate
+print (rate)                        #printing current voice rate
+speaker.setProperty('rate', 125)
+voices = speaker.getProperty('voices')       #getting details of current voice
+speaker.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+# speaker.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female 
+pyttsx3.speak(text)
+speaker.runAndWait()
